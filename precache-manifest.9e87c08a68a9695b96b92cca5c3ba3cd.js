6ff765a7658133a240634039193ba4f8ccd5b172
rim-code <friouirim@gmail.com>
2019-12-03 10:02:38 +0100

    Updates

---
+++ precache-manifest.9e87c08a68a9695b96b92cca5c3ba3cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd7690982694b5c23607b7e0537f3e1",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3c701b219f6c267a12db",
+    "url": "/todolist/static/css/main.a16a8e57.chunk.css"
+  },
+  {
+    "revision": "8418a98f4f2cf13912b1",
+    "url": "/todolist/static/js/2.1650c09a.chunk.js"
+  },
+  {
+    "revision": "3c701b219f6c267a12db",
+    "url": "/todolist/static/js/main.63b17302.chunk.js"
+  },
+  {
+    "revision": "d58f336b7b35c1e64d56",
+    "url": "/todolist/static/js/runtime-main.4ab081a5.js"
+  }
+]);
